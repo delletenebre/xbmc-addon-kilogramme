06a53853efd1da229df83a95d45e3bb2146d976d
Sergei Nezhinskikh <delletenebre@gmail.com>
2020-04-01 20:41:05 +0600

    fix tools

--- release.py
+++ release.py
@@ -1,12 +1,6 @@
-import sys
+from tools import addons_xml_generator, zip_generator
 
-sys.path.append('tools')
-import addons_xml_generator
-import zip_generator
-
-def make_release():
-  addons_xml_generator.Generator()
-  zip_generator.main()
 
 if __name__ == '__main__':
-  make_release()
+  addons_xml_generator.Generator()
+  zip_generator.main()
